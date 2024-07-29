0bdead173b9d03b2e0b14f7054c7edee1ab259a8
GiselleBalieiro <gisellefbalieiro@outlook.com>
2024-07-28 21:07:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const sideMenu = document.querySelector('#sideMenu')
+const navBar = document.querySelector("nav")
+const navLinks = document.querySelector("nav ul")
+
+function openMenu() {
+    sideMenu.style.transform = 'translateX(-16rem)'
+}
+
+function closeMenu() {
+    sideMenu.style.transform = 'translateX(16rem)'
+}
+
+window.addEventListener('scroll', ()=> {
+    if(scrollY > 50) {
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
+        navLinks.classList.remove('bg-white','shadow-sm', 'bg-opacity-50')
+    }
+    else {
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
+        navLinks.classList.add('bg-white','shadow-sm', 'bg-opacity-50')
+    }
+})
