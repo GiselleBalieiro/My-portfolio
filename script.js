9fa4feac980e6fd9f6050689f95bff6d28d5ec6a
Giselle Balieiro <gisellefbalieiro@outlook.com>
2024-12-11 15:31:00 -0300

    feat: dark mode

--- script.js
+++ script.js
@@ -20,3 +20,43 @@ window.addEventListener('scroll', ()=> {
         navLinks.classList.add('bg-white','shadow-sm', 'bg-opacity-50')
     }
 })
+
+//Clique no botão
+var themeToggleDarkIcon = document.getElementById('toggleTheme-dark-icon');
+var themeToggleLightIcon = document.getElementById('toggleTheme-light-icon');
+
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('toggleTheme');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Roboto', 'sans-serif'],
+          heading: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+
+
