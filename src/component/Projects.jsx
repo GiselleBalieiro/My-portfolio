d590cd683c3d59dab085b21a38fcc5854ab21c9d
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-05-03 21:10:36 -0300

    fix: atualização nos projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,7 +1,7 @@
 const Projects = () => {
   return (
     <>
-          <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+          <div id="work" className="w-full px-[12%] py-10 scroll-mt-20 mt-[150px]">
         <h4 className="text-center mb-2 text-lg">My portfolio</h4>
         <h2 className="text-center text-5xl font-mono">My latest projects</h2>
         <p className="text-center max-w-2x1 mx-auto mt-5 mb-12">
@@ -10,8 +10,40 @@ const Projects = () => {
         </p>
 
         <div className="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 gap-5">
+
+        <a href="https://expense-converter-three.vercel.app/" target="_blank">
+            <div className="aspect-square bg-[url('./images/converter.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                <div>
+                  <h2 className="font-semibold">Expense Converter</h2>
+                  <p className="text-sm text-gray-700">Web design</p>
+                </div>
+                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#4e4e4e] group-hover:bg-rose-300 transition">
+                  <img src="./images/send-icon.png" alt="" className="w-5" />
+                </div>
+              </div>
+            </div>
+          </a>
+
+        <a
+            href="https://github.com/GiselleBalieiro/serenatto-project"
+            target="_blank"
+          >
+            <div className="aspect-square bg-[url('./images/Serenatto.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                <div>
+                  <h2 className="font-semibold">Serenatto</h2>
+                  <p className="text-sm text-gray-700">Web design</p>
+                </div>
+                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#4e4e4e] group-hover:bg-rose-300 transition">
+                  <img src="./images/send-icon.png" alt="" className="w-5" />
+                </div>
+              </div>
+            </div>
+          </a>
+
           <a href="https://fokus-project-ten.vercel.app" target="_blank">
-            <div className="aspect-square bg-[url('./images/Captura_fokus.jpeg')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+            <div className="aspect-square bg-[url('./images/teste2.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
               <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
                   <h2 className="font-semibold">Fokus</h2>
@@ -23,14 +55,15 @@ const Projects = () => {
               </div>
             </div>
           </a>
+
           <a
-            href="https://github.com/GiselleBalieiro/Bytebank-project"
+            href="https://product-registration-psi.vercel.app/"
             target="_blank"
           >
-            <div className="aspect-square bg-[url('./images/Captura_bytebank.jpeg')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+            <div className="aspect-square bg-[url('./images/teste.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
               <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
-                  <h2 className="font-semibold">Bytebank</h2>
+                  <h2 className="font-semibold">Product Registration</h2>
                   <p className="text-sm text-gray-700">Web design</p>
                 </div>
                 <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#4e4e4e] group-hover:bg-rose-300 transition">
@@ -39,35 +72,8 @@ const Projects = () => {
               </div>
             </div>
           </a>
-          <a href="https://alura-books-project.vercel.app" target="_blank">
-            <div className="aspect-square bg-[url('./images/Captura_landingpage.jpeg')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-                <div>
-                  <h2 className="font-semibold">LandingPage</h2>
-                  <p className="text-sm text-gray-700">Web design</p>
-                </div>
-                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#4e4e4e] group-hover:bg-rose-300 transition">
-                  <img src="./images/send-icon.png" alt="" className="w-5" />
-                </div>
-              </div>
-            </div>
-          </a>
-          <a
-            href="https://jogo-do-numero-secreto-ashy-theta.vercel.app"
-            target="_blank"
-          >
-            <div className="aspect-square bg-[url('./images/Captura_jogo.jpeg')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-                <div>
-                  <h2 className="font-semibold">Secret Number Game</h2>
-                  <p className="text-sm text-gray-700">Web design</p>
-                </div>
-                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#4e4e4e] group-hover:bg-rose-300 transition">
-                  <img src="./images/send-icon.png" alt="" className="w-5" />
-                </div>
-              </div>
-            </div>
-          </a>
+        
+
         </div>
         <a
           href="https://github.com/GiselleBalieiro"
