d40d087ad3d2cd90c84edb59cee479cdded57a0d
GiselleBalieiro <gisellefbalieiro@outlook.com>
2026-01-03 21:21:44 -0300

    fix: adiciona tech stack na home

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <div className="rounded-xl shadow border border-[#1F2937] p-6 w-full h-full flex flex-col">
+      <>
         <div className="mt-6 mb-6">
           <h1 className="text-[#E5E7EB]">Hi! I'm Giselle Balieiro</h1>   
         </div>
+
         <hr></hr>
+
         <div className="mt-6 mb-6">
           <span className="text-[##CBD5E1]">About Me:  </span>
           <ul className="text-[#9CA3AF] list-disc list-inside space-y-2">
@@ -14,8 +16,9 @@ export default function Home() {
             <li>Web developer with a solid foundation in HTML, CSS and JavaScript, currently deepening knowledge in React.</li>
           </ul>
         </div>
-        
+      
         <hr></hr>
+
         <div className="mt-6 mb-6">
           <span className="text-[##CBD5E1]">Sociais:  </span>
           <div className="flex flex-row gap-3 mt-6 ">
@@ -24,7 +27,51 @@ export default function Home() {
           </div>
         </div>
 
-      </div>
+        <hr></hr>
+
+        <div className="mt-6 mb-6">
+          <span className="text-[##CBD5E1]">Tech Stack: </span>
+          <div className="mt-6">
+            <span className="text-[##CBD5E1]">Front-end</span>
+            <div className="flex flex-row gap-3 mt-2 ">
+              <button className="p-2 bg-[#F97316] rounded-lg">HTML</button>
+              <button className="p-2 bg-[#3B82F6] rounded-lg">CSS</button>
+              <button className="p-2 bg-[#F59E0B] rounded-lg">JavaScript</button>
+              <button className="p-2 bg-[#3B82F6] rounded-lg">jQuery</button>
+              <button className="p-2 bg-[#3178C6] rounded-lg">TypeScript</button>
+              <button className="p-2 bg-[#60A5FA] rounded-lg">React</button>
+              <button className="p-2 bg-[#06B6D4] rounded-lg">TailwindCSS</button>
+            </div>
+          </div>
+          <div className="mt-6">
+            <span className="text-[##CBD5E1]">Back-end</span>
+            <div className="flex flex-row gap-3 mt-2 ">
+              <button className="p-2 bg-[#22C55E] rounded-lg">Node.js</button>
+              <button className="p-2 bg-[#3178C6] rounded-lg">PHP</button>
+            </div>
+          </div>
+          <div className="mt-6">
+            <span className="text-[##CBD5E1]">Data Base</span>
+            <div className="flex flex-row gap-3 mt-2 ">
+              <button className="p-2 bg-[#38BDF8] rounded-lg">MYSQL</button>
+              <button className="p-2 bg-[#3178C6] rounded-lg">SQL</button>
+              <button className="p-2 bg-[#2563EB] rounded-lg">SQLITE</button>
+              <button className="p-2 bg-[#22C55E] rounded-lg">MONGODB ATLAS</button>
+              <button className="p-2 bg-[#8B5CF6] rounded-lg">POSTGRESQL</button>
+            </div>
+          </div>
+          <div className="mt-6">
+            <span className="text-[##CBD5E1]">Software Affinity</span>
+            <div className="flex flex-row gap-3 mt-2 ">
+              <button className="p-2 bg-[#3B82F6] rounded-lg">VSCODE</button>
+              <button className="p-2 bg-[#F97316] rounded-lg">POSTMAN</button>
+              <button className="p-2 bg-[#8B5CF6] rounded-lg">FIGMA</button>
+              <button className="p-2 bg-[#3178C6] rounded-lg">HEIDISQL</button>
+              <button className="p-2 bg-[#3B82F6] rounded-lg">DBEAVER</button>
+            </div>
+          </div>
+        </div>
+      </>
     </div>
   )
 }
